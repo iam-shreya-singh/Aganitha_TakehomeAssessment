a5b588ca3738264cb1dd6f14e9cd406a4cbc8f0f
Shreya Singh <shreyasinghin24@gmail.com>
2025-07-09 19:08:08 +0000

    c

--- get_papers.py
+++ get_papers.py
@@ -1,9 +1,8 @@
-# get_papers.py
 import argparse
-import sys
 from pubmed_fetcher.fetcher import PubMedFetcher
 
-def main():
+def get_papers_list():
+    """Entry point for poetry script"""
     parser = argparse.ArgumentParser(
         description="Fetch research papers from PubMed with non-academic authors from pharma/biotech companies"
     )
@@ -13,17 +12,13 @@ def main():
     
     args = parser.parse_args()
     
-    # Initialize fetcher
     fetcher = PubMedFetcher(
-        email="shreyasinghin24@gmail.com",  # add your email id to access PubMed API
+        email="shreyasinghin24@gmail.com",
         debug=args.debug
     )
     
-    # Fetch papers
     papers = fetcher.fetch_papers(args.query)
-    
-    # Write results
     fetcher.write_csv(papers, args.file)
 
 if __name__ == "__main__":
-    main()
+    get_papers_list()
