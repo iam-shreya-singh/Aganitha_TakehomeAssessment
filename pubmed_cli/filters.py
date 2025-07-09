3faa211bb16b846fb299677b8d9b483f138c940d
Shreya Singh <shreyasinghin24@gmail.com>
2025-07-09 14:50:44 +0000

    csv export

--- pubmed_cli/filters.py
+++ pubmed_cli/filters.py
@@ -1,13 +1,13 @@
-def is_non_academic_journal(affiliation: str) -> bool:
+def is_non_academic(affiliation: str) -> bool:
 
 academic_keywords = ["university","college", "institute","hospital", "school", "center"]
 company_keywords =["ltd", "inc", "corp", "pharma", "biotech", "biotechnology", "pharmaceutical", "laboratory"]
 
 
 aff_lower = affiliation.lower()
-if any(keyword in aff_lower for keyword in academic_keywords):
+if any(word in aff_lower for word in academic_keywords):
     return False
-if any(keyword in aff_lower for keyword in company_keywords):
+if any(word in aff_lower for word in company_keywords):
     return True
 return False
 
